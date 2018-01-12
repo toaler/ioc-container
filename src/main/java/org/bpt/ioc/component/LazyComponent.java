757e01541ddd520361cd26e2fcd36d40b8bf595c
Brian <btoal@salesforce.com>
2018-01-11 17:05:11 -0800

    Added documentation regarding the behavior of @Lazy applied at different scopes; method vs method args

--- src/main/java/org/bpt/ioc/component/LazyComponent.java
+++ src/main/java/org/bpt/ioc/component/LazyComponent.java
@@ -4,27 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-@Component(value="LazyComponent")
+@Component(value = "LazyComponent")
 @Lazy
 public class LazyComponent {
-	
+
 	public LazyComponent() {
 		System.out.println("ctor LazyComponent");
 	}
 
-	@Autowired @Lazy
+	@Autowired
+	@Lazy
 	private LazyAutoWired lazyAutoWired;
 
 	@Autowired
 	private EagerAutoWired eagerAutoWired;
-	
+
 	private LazyAutoWiredViaSetter lazyAutoWiredViaSetter;
-	
+
+	/**
+	 * The @Lazy annotation can be used either at the method or method argument
+	 * levels. I suspect if put at the method level, all member args are tried
+	 * as @Lazy proxies, while using per method argument allows proxies to be
+	 * specified at a finer granularity.  I haven't validate this yet.
+	 * 
+	 * @param lazyAutoWiredViaSetter
+	 */
 	@Autowired
 	public void setLazyAutoWiredViaSetter(@Lazy LazyAutoWiredViaSetter lazyAutoWiredViaSetter) {
-		this.lazyAutoWiredViaSetter = lazyAutoWiredViaSetter; 
+		this.lazyAutoWiredViaSetter = lazyAutoWiredViaSetter;
 	}
-	
+
 	public LazyAutoWiredViaSetter getLazyAutoWiredViaSetter() {
 		return lazyAutoWiredViaSetter;
 	}
